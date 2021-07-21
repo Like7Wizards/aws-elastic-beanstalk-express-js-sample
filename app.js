0ef36d3b002e681a8bbdb3f6ee0dc2e42a4018f4
Like7Wizards <craigcoughtrie@gmail.com>
2021-07-21 18:58:32 +0100

    Full pipeline test2

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Bark Bork!'));
+app.get('/', (req, res) => res.send('<html><title>Awesome AWS Codepipeline test</title><H1>We are the championes</H1></HTML>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
