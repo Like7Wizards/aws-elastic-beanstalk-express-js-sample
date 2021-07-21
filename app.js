5a1a247164b5370e206539047cb99f02f7ac6890
Like7Wizards <craigcoughtrie@gmail.com>
2021-07-21 18:53:43 +0100

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Ye Wee BawBag'));
+app.get('/', (req, res) => res.send('Bark Bork!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
