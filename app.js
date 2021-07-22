85a6063ae07b83cb13e413ddb068f88a814c487a
Like7Wizards <craigcoughtrie@gmail.com>
2021-07-22 16:05:14 +0100

    change message

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('<html><title>Awesome AWS Codepipeline test</title><H1>We are the championes</H1></HTML>'));
+app.get('/', (req, res) => res.send('<title>Well well well</title>
+<body>What do we have here?
+<p>This seems to be relatively easy to pick up I suppose</p>
+<p>Though I'm sure it become much more complicated</p>
+</body>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
