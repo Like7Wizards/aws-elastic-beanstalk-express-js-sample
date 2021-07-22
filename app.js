197a221f18ac427054c78826e543bf2c636da308
Like7Wizards <craigcoughtrie@gmail.com>
2021-07-22 16:47:04 +0100

    change message

--- app.js
+++ app.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('<title>WTF!?</title>
-<img src="Hehehe.png">'));
+app.get('/', (req, res) => res.send('<title> Wowzers!</title><Body>Wowzers is right!</body>'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
